e24e00598127fa8a6a0d1b58c8c5259700e73566
PHANNA11 <phaphanna2018@gmail.com>
2024-03-24 16:36:42 +0700

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_project/src/view/auth/login_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  get _getInitFirebase {
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Scaffold(
+            body: Center(
+              child: Icon(
+                Icons.info,
+                size: 35,
+                color: Colors.red,
+              ),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const LoginScreen();
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +43,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: _getInitFirebase,
     );
   }
 }
--- lib/src/view/auth/login_screen.dart
+++ lib/src/view/auth/login_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_project/src/view/home/home_screen.dart';
 import 'package:firebase_project/src/widget/textfield_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +15,8 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   late bool isHintPassword = true;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,14 +25,16 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FlutterLogo(
+            const FlutterLogo(
               size: 200,
             ),
             TextFielWidget(
+              controller: emailController,
               prefixIcon: Icons.email,
               hintText: 'Enter E-mail',
             ),
             TextFielWidget(
+              controller: passwordController,
               prefixIcon: Icons.lock_open_rounded,
               hintText: 'Enter password',
               hintPassword: isHintPassword,
@@ -45,17 +53,88 @@ class _LoginScreenState extends State<LoginScreen> {
               child: CupertinoButton(
                   color: Colors.blueAccent,
                   child: const Text('Sign In'),
-                  onPressed: () {}),
+                  onPressed: () async {
+                    try {
+                      final credential = await FirebaseAuth.instance
+                          .signInWithEmailAndPassword(
+                        email: emailController.text,
+                        password: passwordController.text,
+                      );
+
+                      if (credential.user!.getIdToken() != null) {
+                        // ignore: use_build_context_synchronously
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomeScreen()));
+                      }
+                    } on FirebaseAuthException catch (e) {
+                      log(e.credential!.providerId);
+                      if (e.code == 'user-not-found') {
+                        print('No user found for that email.');
+                      } else if (e.code == 'wrong-password') {
+                        print('Wrong password provided for that user.');
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+                  }),
             ),
-            Spacer(),
+            // TODO : Login with google and Facebook
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10, horizontal: 100),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: const CircleAvatar(
+                      child: Icon(
+                        Icons.facebook,
+                        size: 40,
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: const CircleAvatar(
+                      child: Icon(
+                        Icons.language,
+                        size: 40,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Spacer(),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
+                const Text(
                   'If you have account!.',
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                TextButton(onPressed: () {}, child: Text("Create account"))
+                TextButton(
+                    onPressed: () async {
+                      try {
+                        final credential = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                          email: emailController.text,
+                          password: passwordController.text,
+                        );
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'weak-password') {
+                          print('The password provided is too weak.');
+                        } else if (e.code == 'email-already-in-use') {
+                          print('The account already exists for that email.');
+                        }
+                      } catch (e) {
+                        print(e);
+                      }
+                    },
+                    child: const Text("Create account"))
               ],
             )
           ],
