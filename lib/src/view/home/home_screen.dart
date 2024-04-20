31944f0c5d226c84305b5a4e89b595a9cbb6ce8e
PHANNA11 <phaphanna2018@gmail.com>
2024-04-20 15:57:16 +0700

    CRUD Firestore

--- lib/src/view/home/home_screen.dart
+++ lib/src/view/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_project/src/view/auth/login_screen.dart';
+import 'package:firebase_project/src/view/course/create_course_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -27,6 +28,21 @@ class HomeScreen extends StatelessWidget {
               },
               title: const Text('Sign Out'),
               trailing: const Icon(Icons.logout),
+            ),
+            ListTile(
+              onTap: () async {
+                Navigator.pop(context);
+                await Future.delayed(const Duration(milliseconds: 200))
+                    .whenComplete(() => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AddEditCourseScreen(
+                                  courseData: null,
+                                  refId: '',
+                                ))));
+              },
+              title: const Text('Create Course'),
+              trailing: const Icon(Icons.file_copy),
             )
           ]),
         ),
@@ -54,7 +70,8 @@ class HomeScreen extends StatelessWidget {
                 itemBuilder: (context, index) {
                   var data =
                       snapshot.data!.docs[index].data() as Map<String, dynamic>;
-                  return courseCard(data: data);
+                  return courseCard(context,
+                      data: data, refId: snapshot.data!.docs[index].id);
                 },
                 separatorBuilder: (context, index) => Divider(),
                 itemCount: snapshot.data!.docs.length);
@@ -64,38 +81,74 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  Widget courseCard({required Map data}) {
+  Widget courseCard(BuildContext context,
+      {required Map data, required String refId}) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Card(
-        child: Container(
-          color: Colors.white,
-          child: Row(
-            children: [
-              CachedNetworkImage(
-                height: 120,
-                width: 120,
-                imageUrl: data['image'],
-                progressIndicatorBuilder: (context, url, downloadProgress) =>
-                    Center(
-                        child: CircularProgressIndicator(
-                            value: downloadProgress.progress)),
-                errorWidget: (context, url, error) => const Icon(Icons.error),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.white,
+              child: Row(
                 children: [
-                  Text(
-                    data['course_name'],
+                  CachedNetworkImage(
+                    height: 120,
+                    width: 120,
+                    imageUrl: data['image'],
+                    progressIndicatorBuilder:
+                        (context, url, downloadProgress) => Center(
+                            child: CircularProgressIndicator(
+                                value: downloadProgress.progress)),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
                   ),
-                  Text(
-                    "\$ ${double.parse(data['price'].toString())}",
-                    style: const TextStyle(color: Colors.red),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        data['course_name'],
+                      ),
+                      Text(
+                        "\$ ${double.parse(data['price'].toString())}",
+                        style: const TextStyle(color: Colors.red),
+                      ),
+                    ],
                   )
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+            Positioned(
+                right: 0,
+                height: 120,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () async {
+                          await dataRef.doc(refId).delete();
+                        },
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AddEditCourseScreen(
+                                        courseData: data,
+                                        refId: refId,
+                                      )));
+                        },
+                        icon: const Icon(
+                          Icons.edit_document,
+                          color: Colors.blueAccent,
+                        )),
+                  ],
+                ))
+          ],
         ),
       ),
     );
